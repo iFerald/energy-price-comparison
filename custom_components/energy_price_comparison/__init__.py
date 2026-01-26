8bf757ea27d246f5df786a01995cc7a5212d7327
iFerald <fwolcz@gmail.com>
2026-01-26 19:50:06 +0000

    Update __init__.py

--- custom_components/energy_price_comparison/__init__.py
+++ custom_components/energy_price_comparison/__init__.py
@@ -3,13 +3,10 @@ from __future__ import annotations
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
-from .const import DOMAIN
-
 PLATFORMS: list[str] = ["sensor"]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    hass.data.setdefault(DOMAIN, {})
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
